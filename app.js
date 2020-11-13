3c358c14455662196922395f43e267c69f63b400
x3Dima <x3dima.romanenko@gmail.com>
2020-11-13 22:36:56 +0200

    add LocalStorage and fix bag

--- app.js
+++ app.js
@@ -1,3 +1,12 @@
+const modal = document.querySelector(".modal");
+let modalScore = document.querySelector(".modal-score");
+let userName = document.querySelector(".input-name");
+const gameBlock = document.querySelector(".game-block");
+let resultList = document.querySelector(".result-list");
+const saveResultsBtn = document.querySelector(".save-results");
+
+let counter = 0;
+
 function gameLogic() {
   // Переменные
   const gameContainer = document.querySelector(".game-container");
@@ -5,7 +14,6 @@ function gameLogic() {
 
   let cubesCount = 1500;
   const cubesArray = [];
-  let counter = 0;
   // Создание элемента
   function createCube() {
     const div = document.createElement("div");
@@ -73,8 +81,6 @@ gameLogic();
 // Таймер
 
 function initCountdown() {
-  const gameBlock = document.querySelector(".game-block");
-
   // Перезапуск игры
   function event_click_cancel(event) {
     pause();
@@ -106,7 +112,8 @@ function initCountdown() {
     let [minutes, seconds] = time;
     if (seconds == 0) {
       clearInterval(interval);
-      location.reload();
+
+      gameOver();
     }
     if (seconds < 10) {
       table.innerHTML = `00:0${seconds}`;
@@ -129,7 +136,7 @@ function initCountdown() {
   const toggleElement = document.querySelector(".start");
   const cancelElement = document.querySelector(".new-game");
   let interval = null;
-  let time = [01, 00];
+  let time = [0o1, 0o0];
 
   // События
   toggleElement.addEventListener("click", event_click_startpause);
@@ -138,4 +145,57 @@ function initCountdown() {
 }
 initCountdown();
 
-// Вывод результатов
+// Всплытие модального окна
+function gameOver() {
+  modalScore.innerHTML = counter;
+  modal.style.display = "block";
+  gameBlock.classList.add("block");
+}
+// Функция создает обьект с результатом
+saveResultsBtn.addEventListener("click", () => {
+  let resultData = {
+    name: userName.value,
+    score: counter,
+  };
+
+  saveLocalResults(resultData);
+});
+// Функция сохраняет имя и результат в localStorage
+function saveLocalResults(res) {
+  let resultsArray;
+  if (localStorage.getItem("resultsArray") === null) {
+    resultsArray = [];
+  } else {
+    resultsArray = JSON.parse(localStorage.getItem("resultsArray"));
+  }
+
+  resultsArray.push(res);
+  localStorage.setItem("resultsArray", JSON.stringify(resultsArray));
+}
+function getResultsFromLocal() {
+  if (localStorage.getItem("resultsArray") === null) {
+    resultsArray = [];
+  } else {
+    resultsArray = JSON.parse(localStorage.getItem("resultsArray"));
+  }
+  resultsArray
+    .sort((a, b) => {
+      return b.score - a.score;
+    })
+    .forEach((elem, i) => {
+      if (i < 7) {
+        const div = document.createElement("div");
+        div.classList.add("achievements");
+        const span = document.createElement("span");
+        span.classList.add("user-name");
+        span.textContent = elem.name;
+        const strong = document.createElement("strong");
+        strong.classList.add("achievement");
+        strong.textContent = elem.score;
+        div.appendChild(span);
+        div.appendChild(strong);
+        resultList.appendChild(div);
+      }
+    });
+}
+getResultsFromLocal();
